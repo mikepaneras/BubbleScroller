c91195360cfdb7949b9bd272b508068a9135a6c8
mikepaneras <mikepaneras21@gmail.com>
2025-01-25 01:15:34 +0200

    -energy bar
    adsadasd

---
+++ Assets/Scripts/EnergyBar.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyBar : MonoBehaviour
+{
+    [SerializeField] Image barFillImage;
+    const float TOTAL_ENERGY = 100;
+    float currentEnergy;
+    [SerializeField] float depletionSpeed = 1;
+    [SerializeField] float refillSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentEnergy = TOTAL_ENERGY;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Pressing Space");
+            if(currentEnergy > 0 )
+            {
+                currentEnergy -= Time.deltaTime * depletionSpeed;
+                if (currentEnergy < 0)
+                {
+                    currentEnergy = 0;
+                }
+            }
+        }
+        else
+        {
+            if(currentEnergy < TOTAL_ENERGY)
+            {
+                currentEnergy += Time.deltaTime * refillSpeed;
+                if(currentEnergy > TOTAL_ENERGY)
+                {
+                    currentEnergy = TOTAL_ENERGY;
+                }
+            }
+        }
+        barFillImage.fillAmount =  currentEnergy / TOTAL_ENERGY;
+    }
+    public float GetCurrentEnergy() => currentEnergy;
+}
