d7a83585e33ef4d97105b37e59e9cd2292cfbc97
Nestoras Angelopoulos <nestorasangelopoulos@gmail.com>
2025-01-25 18:57:55 +0200

    Fixed player rotation

--- Assets/Scripts/Player/PlayerCollisionManager.cs
+++ Assets/Scripts/Player/PlayerCollisionManager.cs
@@ -8,6 +8,11 @@ public class PlayerCollisionManager : MonoBehaviour
         player = transform.parent.GetComponent<Player>();
     }
 
+    private void LateUpdate()
+    {
+        transform.rotation = Quaternion.identity;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Death"))
@@ -22,6 +27,7 @@ public class PlayerCollisionManager : MonoBehaviour
 
                 // Move player to center
                 player.transform.position -= new Vector3(player.transform.position.x, 0,0);
+                player.transform.rotation = Quaternion.identity;
             }
         }
         else if (collision.gameObject.tag == "GoldenBubble")
