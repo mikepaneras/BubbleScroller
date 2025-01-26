bcb2d4253c92e013e1df9f01f223adfd00a49e6c
SAEthanos <13-13885@saeinstitute.edu>
2025-01-26 16:28:50 +0200

    outro
    made outro in fade script

--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -7,10 +7,12 @@ using UnityEngine.UI;
 public class Intro : MonoBehaviour
 {
     bool startFade = false;
+    bool startIntro = false;
     public Image image;
     public TextMeshProUGUI textMeshPro;
 
-
+    public Image outroimage;
+    public TextMeshProUGUI textoutro;
     void Start()
     {
         Invoke("FadeOut", 6f);
@@ -21,12 +23,23 @@ public class Intro : MonoBehaviour
         startFade = true;
     }
 
+    void FadeIN()
+    {
+        startIntro = true;
+    }
+
     void Update()
     {
         if (startFade)
         {
             image.color -= new Color(0,0,0, Time.deltaTime);
             textMeshPro.color -= new Color(0, 0, 0, Time.deltaTime);
+        }
+
+        if (startIntro)
+        {
+            image.color += new Color(0, 0, 0, Time.deltaTime);
+            textMeshPro.color += new Color(0, 0, 0, Time.deltaTime);
         }
     }
 }
