ad7e5dad3ff706c9bf473cef5d631effe20c765d
mikepaneras <13-12934@saeinstitute.edu>
2025-01-27 22:16:48 +0200

    -messages

--- Assets/Scripts/Message.cs
+++ Assets/Scripts/Message.cs
@@ -9,21 +9,33 @@ public class Message : MonoBehaviour
     [SerializeField] string messageText;
     [SerializeField] bool hasDisplayTimer;
     [SerializeField] float displayTimer;
+    float counter = 0;
     bool triggerPassed = false;
     // Start is called before the first frame update
     void Start()
     {
         messageObject.text = messageText;
+        if(hasDisplayTimer)
+        {
+            messageObject.color = new Color(1, 1, 1, 0);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         if(triggerPassed && hasDisplayTimer)
-        {
+        {       
             if(displayTimer > 0)
             {
+                messageObject.color += new Color(0, 0, 0, Time.deltaTime);
                 displayTimer -= Time.deltaTime;
+                return;
+            }
+            if(messageObject.color.a > 0)
+            {
+                messageObject.color -= new Color(0, 0, 0, Time.deltaTime);
+                counter += Time.deltaTime;
                 return;
             }
             Destroy(gameObject);
@@ -31,7 +43,10 @@ public class Message : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        triggerPassed = true;
-        messageObject.gameObject.SetActive(true);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            triggerPassed = true;
+            messageObject.gameObject.SetActive(true);
+        }
     }
 }
